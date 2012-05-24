d01b4ae4e414f0ee1491fc813789cd762e511050
Peter Jaraczewski <peter@wikipeter.org>
2012-05-24 23:58:56 +0200

    added possibility for setting custom environment variables

---
+++ Environment/local_database.php
@@ -0,0 +1,9 @@
+<?php
+
+// custom settings vor local development environment
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'local_username';
+$db['default']['password'] = 'local_password';
+$db['default']['database'] = 'local_database';
+
+?>
