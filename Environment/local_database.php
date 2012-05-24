8887331b8b5ba76de03e5c65e3acc0a3b29099cf
Peter Jaraczewski <peter@wikipeter.org>
2012-05-25 01:15:29 +0200

    adjusted comment for local database config file & removed local database file from repository

--- Environment/local_database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// custom settings vor local development environment
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
-$db['default']['database'] = 'meinFHD';
-
-?>
